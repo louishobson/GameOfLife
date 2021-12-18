acf819cdad095da7b7cccfca8e55f87664f12564
ldh35 <ldh35@cam.ac.uk>
2021-12-18 23:48:22 +0000

    Macros.asm: Added macros to a separate file.

---
+++ Macros.asm
@@ -0,0 +1,44 @@
+; Include once
+#ifndef MACROS_INCLUDE_ONCE
+#define MACROS_INCLUDE_ONCE
+
+; The load position of the program
+#define START 60000
+
+; The position of the worlds.
+; Each world consists of 24 rows of 4 bytes, making 96 bytes in total.
+; There also needs to be padding of 4 zero bytes before and after each.
+; Therefore if we can set the worlds 100 bytes apart, so that they share some padding.
+#define WORLD1 62000
+#define WORLD2 62100
+
+; The position of the rule set.
+; Rather than packing the rule set, we just store 18 booleans as bytes.
+; We write true as 255 and false as 0.
+; True means alive in the next generation, false means dead.
+; The first 9 bytes define the rules for a living cell with 0 through 8 neighbors.
+; The second 9 bytes define the same for a dead cell.
+; Note that 62720 = 0xf500 in hex, so to get a rule with a number of neighbors, we load the number of neighbors into the bottom byte of the address.
+#define RULES 62720
+#define RULES_UPPER $f5
+
+; Set to 0 for dead as black, and white as alive.
+; Set to 255 for dead as white, and alive as black.
+#define COLOR_FLIP 62738
+
+; Define the position of the character bitmaps in memory.
+#define CHAR_BITMAPS $3c00
+
+; Define the position of the pixel data in memory, and the length of the data.
+#define PIXEL_DATA $4000
+#define PIXEL_DATA_LENGTH $1800
+#define PIXEL_DATA_LENGTH_UPPER $18
+
+; Define the position of the attribute data in memory, and length of the data
+#define ATTRIBUTE_DATA $5800
+#define ATTRIBUTE_DATA_UPPER $58
+#define ATTRIBUTE_DATA_LENGTH $0300
+#define ATTRIBUTE_DATA_LENGTH_UPPER $03
+
+; End of pragma once
+#endif
