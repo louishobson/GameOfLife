4ff2f41f01fd01e9664f7efcd716e1a2b20951d3
ldh35 <ldh35@cam.ac.uk>
2021-12-19 04:12:47 +0000

    Macros.asm: Minor modifications and extra macros.

--- Macros.asm
+++ Macros.asm
@@ -9,8 +9,8 @@
 ; Each world consists of 24 rows of 4 bytes, making 96 bytes in total.
 ; There also needs to be padding of 4 zero bytes before and after each.
 ; Therefore if we can set the worlds 100 bytes apart, so that they share some padding.
-#define WORLD1 62000
-#define WORLD2 62100
+#define WORLD1 62520
+#define WORLD2 62620
 
 ; The position of the rule set.
 ; Rather than packing the rule set, we just store 18 booleans as bytes.
@@ -22,9 +22,14 @@
 #define RULES 62720
 #define RULES_UPPER $f5
 
+; A toggle for swapping alive and dead colors.
 ; Set to 0 for dead as black, and white as alive.
 ; Set to 255 for dead as white, and alive as black.
 #define COLOR_FLIP 62738
+
+; The current timer for automatic generation switching.
+; The fastest is 1, and off is 256 (equals 0).
+#define AUTO_GEN_TIMER 62739
 
 ; Define the position of the character bitmaps in memory.
 #define CHAR_BITMAPS $3c00
@@ -39,6 +44,9 @@
 #define ATTRIBUTE_DATA_UPPER $58
 #define ATTRIBUTE_DATA_LENGTH $0300
 #define ATTRIBUTE_DATA_LENGTH_UPPER $03
+
+; The keyboard's id
+#define KEYBOARD_IN_ID $fe
 
 ; End of pragma once
 #endif
